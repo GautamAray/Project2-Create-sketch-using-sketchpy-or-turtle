e2bed26783838e8ea33ea8c4120e31a5efba188d
GautamAray <140047058+GautamAray@users.noreply.github.com>
2023-07-23 12:56:27 +0530

    Add files via upload

--- FirstProject/sketch.py
+++ FirstProject/sketch.py
@@ -1,3 +1,7 @@
-from sketchpy import canvas
-obj = canvas.sketch_from_image(r"C:\Users\91970\Downloads\akshay kumar.jpg")
-obj.draw(threshold = 90)
+from sketchpy import library as lib
+obj = lib.apj()
+obj.draw()
+
+# from sketchpy import canvas
+# obj = canvas.sketch_from_image(r"C:\Users\91970\Downloads\akshay kumar.jpg")
+# obj.draw(threshold = 150)
